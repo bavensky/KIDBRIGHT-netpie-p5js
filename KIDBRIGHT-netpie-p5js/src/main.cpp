3f248b266f7011432f78c7877ac72fc6de31fde3
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-08-29 15:54:36 +0700

    add mqtt-connecontor

--- KIDBRIGHT-netpie-p5js/src/main.cpp
+++ KIDBRIGHT-netpie-p5js/src/main.cpp
@@ -1,9 +1,77 @@
 #include <Arduino.h>
+#include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+#ifdef ESP8266 
+  #include <ESP8266WiFi.h>
+#else 
+  #include <WiFi.h>
+#endif
+
+#include <ArduinoJson.h>
+#include <MqttConnector.h>
+#include <Wire.h>
+#include <SPI.h>
+
+#include "init_mqtt.h"
+#include "_publish.h"
+#include "_receive.h"
+#include "_config.h"
+
+#define S1  16
+#define S2  14
+
+MqttConnector *mqtt; 
+
+int relayPin = 26; 
+int relayPinState = HIGH;
+char myName[40];
+int x, y;
+uint32_t btCount = 0;
+
+void init_hardware()
+{
+  pinMode(relayPin, OUTPUT);
+  pinMode(2, OUTPUT);
+
+  pinMode(S1, INPUT_PULLUP);
+  pinMode(S2, INPUT_PULLUP);
+
+  digitalWrite(relayPin, relayPinState);;
+  // serial port initialization
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println("Starting...");
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void init_wifi() {
+  WiFi.disconnect();
+  delay(20);
+  WiFi.mode(WIFI_STA);
+  delay(50);
+  const char* ssid =  WIFI_SSID.c_str();
+  const char* pass =  WIFI_PASSWORD.c_str();
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf ("Connecting to %s:%s\r\n", ssid, pass);
+    delay(300);
+  }
+  Serial.println("WiFi Connected.");
+  digitalWrite(2, HIGH);
+}
+
+void setup()
+{
+  init_hardware();
+  init_wifi();
+  init_mqtt();
+}
+
+void loop()
+{
+  if(digitalRead(S1) == 0) {
+    delay(200);
+    btCount += 1;
+  }
+  mqtt->loop();
 }
